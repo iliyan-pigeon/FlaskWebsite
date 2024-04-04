d58821086f3afee7b1bcc5719476848816c1b5e1
Iliyan Galabov <milenov556@gmail.com>
2024-04-04 11:03:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello! This is the main page <h1>HELLO<h1>"
+
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
